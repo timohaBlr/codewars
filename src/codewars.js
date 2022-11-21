206b35826721cd44dc5799f38553025a8fa4c4ed
timohaBlr <94timoha@gmail.com>
2022-11-21 11:34:34 +0300

    7 kyu Coloured Triangles solution with recursion

--- src/codewars.js
+++ src/codewars.js
@@ -1,19 +1,38 @@
-function partlist(arr) {
-    let firstArr = [];
-    let resulArr = [];
-    let answer = [];
-    while (arr.length > 1) {
-        firstArr.push(arr.shift())
-resulArr = [firstArr.join(' '), arr.join(' ')]
-        answer.push(resulArr)
-       // console.log(firstArr.join(' '))
-       // console.log(arr.join(' '))
-
-      //console.log(resulArr)
+function triangle(row) {
+    if (row.length === 1) return row;
+      for (let i = 0; i < row.length - 1; i++) {
+        let supposed = row.slice(i, i + 2)
+        let letter = '';
+        switch (supposed) {
+            case 'RB':
+            case 'BR':
+                letter = 'G';
+                break;
+            case 'RG':
+            case 'GR':
+                letter = 'B';
+                break;
+            case 'BG':
+            case 'GB':
+                letter = 'R';
+                break;
+            case 'BB':
+                letter = 'B';
+                break;
+            case 'RR':
+                letter = 'R';
+                break;
+            case 'GG':
+                letter = 'G';
+                break;
+        }
+        row = '';
+        row += letter;
+       // console.log(row)
     }
-return answer;
+    triangle(row)
+    return row;
 }
 
-console.log(partlist(["az", "toto", "picaro", "zone", "kiwi"]))
-
-partlist(["az", "toto", "picaro", "zone", "kiwi"])
+triangle('RBRGBRBGGRRRBGBBBGG');
+console.log(triangle('RGBG'))
