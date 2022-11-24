af7432f6139320ff5d922f709adf4a7e99dabee3
timohaBlr <94timoha@gmail.com>
2022-11-24 23:15:55 +0300

    6 kyu Bouncing Balls

--- src/codewars.js
+++ src/codewars.js
@@ -1,47 +1,25 @@
-function incrementString(str) {
-    const findIndexOfLastLetter = (str) => {
-        let arr = str.split('')
-        arr.reverse()
-        arr.map(m => +m)
-        let counter = 0;
-        while (!isNaN(arr[0])) {
-            arr.shift()
-            counter++;
+function bouncingBall(h,  bounce,  window) {
+    if (h<=0 || 0 >= bounce || bounce >= 1 || window >= h ) {
+        return -1;
+    } else if ( h * bounce < window) {
+        return 1;
+    } else {
+        let result = 0;
+        while (h *bounce > window) {
+            result += 2;
+            h = h* bounce
         }
-        return str.length - 1 - counter
+        return result+1
     }
-    let lastLetterIndex = findIndexOfLastLetter(str)
-    let arr = str.split('');
-    let index = lastLetterIndex + 1
-    let firstNum = arr[index]
 
-    if (firstNum) {
-        let strPart = arr.slice(0, index).join('');
-        let numPart = arr.slice(index).join('');
-        console.log(strPart, numPart)
-        let numPartLength = arr.length - strPart.length
-        numPart++;
-        let differ = numPartLength - String(numPart).length
-        for (let i = 0; i < differ; i++) {
-            strPart += '0'
-        }
-        return strPart + numPart;
-        console.log(strPart)
-
-    } else return str + '1';
 }
 
+console.log(bouncingBall(30.0, 0.66, 1.5))
 
-console.log('function return:' + ' ' + incrementString('fo99obar979'))
-//arr.indexOf(item, from)
+//Float parameter "h" in meters must be greater than 0
+//Float parameter "bounce" must be greater than 0 and less than 1
+//Float parameter "window" must be less than h.
+//- h = 3, bounce = 0.66, window = 1.5, result is 3
 
-/*
-doTest("foobar000", "foobar001");
-doTest("foobar999", "foobar1000");
-doTest("foobar00999", "foobar01000");
-doTest("foo", "foo1");
-doTest("foobar001", "foobar002");
-doTest("foobar1", "foobar2");
-doTest("1", "2");
-doTest("009", "010");
-doTest("fo99obar99", "fo99obar100");*/
+//- h = 3, bounce = 1, window = 1.5, result is -1
+//(Condition 2) not fulfilled)
