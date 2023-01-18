21e98b36f6a0a089812ad0c3e49c71dec2c8d7b0
timohaBlr <94timoha@gmail.com>
2023-01-18 13:08:42 +0300

    5 kyu Valid Parentheses

--- src/codewars.js
+++ src/codewars.js
@@ -1,13 +1,17 @@
-function pigIt(str) {
-    return str.split(' ')
-        .map(m => (m !== '?' && m !== ',' && m !== '.' && m !== '!')
-            ? m.slice(1) + m.slice(0, 1) + 'ay'
-            : m
-        )
-        .join(' ')
+function validParentheses(parens) {
+let arr = parens.split(',')
+    return arr;
 }
 
-console.log(pigIt('Pig latin is cool')) //igPay atinlay siay oolcay
-console.log(pigIt('Hello world !')) //elloHay orldway !
-// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+console.log('sdas')
+console.log(validParentheses( "(" )) //false
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+//
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+// Constraints
+// 0 <= input.length <= 100
 
